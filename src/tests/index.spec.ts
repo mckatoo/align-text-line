99115a7c9a253f5b8cfc1b7966dedf9364a1e361
Milton Carlos Katoo <mckatoo@gmail.com>
2020-07-18 11:36:19 -0300

    test: add commitizen commit-lint lint-staged husky and jest
    re #1

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,14 @@
+describe('Align Text', () => {
+
+  it('Left Align', async () => {
+    expect(true).toBe(true)
+  })
+
+  it('Align Right', async () => {
+    expect(true).toBe(true)
+  })
+
+  it('Center Align', async () => {
+    expect(true).toBe(true)
+  })
+})
