0c313d5820454cb76e6905f82cd57bc290609427
Milton Carlos Katoo <mckatoo@gmail.com>
2020-07-18 23:53:13 -0300

    refactor: add delimiter with parameter
    close #2

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,84 @@
-export function centralized(text: string, lineSize: number): string {
-  const initText = lineSize / 2 - text.length / 2 - 1
-  const endText = initText + text.length + 1
-  let cursorTextPosition = 0
-  let alignedText = ''
+class Align {
+  public centralized(text: string, delimiter:string, lineSize: number): string {
+    const initText = lineSize / 2 - text.length / 2 - 1
+    const endText = initText + text.length + 1
+    let cursorTextPosition = 0
+    let alignedText = ''
 
-  for (let x = 0; x < lineSize; x++) {
-    if (x > initText && x < endText) {
-      alignedText += text[cursorTextPosition]
-      cursorTextPosition++
-    } else {
-      alignedText += '-'
+    for (let x = 0; x < lineSize; x++) {
+      if (x > initText && x < endText) {
+        alignedText += text[cursorTextPosition]
+        cursorTextPosition++
+      } else {
+        alignedText += delimiter
+      }
     }
+
+    return alignedText
   }
 
-  return alignedText
-}
+  public rightAligned(text: string, delimiter:string, lineSize: number): string {
+    const initText = lineSize - text.length - 1
+    const endText = initText + text.length + 1
+    let cursorTextPosition = 0
+    let alignedText = ''
 
-export function rightAligned(text: string, lineSize: number): string {
-  const initText = lineSize - text.length - 1
-  const endText = initText + text.length + 1
-  let cursorTextPosition = 0
-  let alignedText = ''
-
-  for (let x = 0; x < lineSize; x++) {
-    if (x > initText && x < endText) {
-      alignedText += text[cursorTextPosition]
-      cursorTextPosition++
-    } else {
-      alignedText += '-'
+    for (let x = 0; x < lineSize; x++) {
+      if (x > initText && x < endText) {
+        alignedText += text[cursorTextPosition]
+        cursorTextPosition++
+      } else {
+        alignedText += delimiter
+      }
     }
+
+    return alignedText
   }
 
-  return alignedText
-}
+  public leftAligned(text: string, delimiter:string, lineSize: number): string {
+    let cursorTextPosition = 0
+    let alignedText = ''
 
-export function leftAligned(text: string, lineSize: number): string {
-  let cursorTextPosition = 0
-  let alignedText = ''
-
-  for (let x = 0; x < lineSize; x++) {
-    if (x >= 0 && x < text.length) {
-      alignedText += text[cursorTextPosition]
-      cursorTextPosition++
-    } else {
-      alignedText += '-'
+    for (let x = 0; x < lineSize; x++) {
+      if (x >= 0 && x < text.length) {
+        alignedText += text[cursorTextPosition]
+        cursorTextPosition++
+      } else {
+        alignedText += delimiter
+      }
     }
+
+    return alignedText
   }
 
-  return alignedText
-}
+  public leftRightAligned(
+    textLeft: string,
+    textRight: string,
+    delimiter: string,
+    lineSize: number
+  ): string {
+    let cursorTextLeftPosition = 0
+    const initTextRight = lineSize - textRight.length - 1
+    const endTextRight = initTextRight + textRight.length + 1
+    let cursorTextRightPosition = 0
+    let alignedText = ''
 
-export function leftRightAligned(
-  textLeft: string,
-  textRight: string,
-  lineSize: number
-): string {
-  let cursorTextLeftPosition = 0
-  const initTextRight = lineSize - textRight.length - 1
-  const endTextRight = initTextRight + textRight.length + 1
-  let cursorTextRightPosition = 0
-  let alignedText = ''
+    if (lineSize < textRight.length + textLeft.length) return Error.toString()
 
-  if (lineSize < textRight.length + textLeft.length) return Error.toString()
-
-  for (let x = 0; x < lineSize; x++) {
-    if (x >= 0 && x < textLeft.length) {
-      alignedText += textLeft[cursorTextLeftPosition]
-      cursorTextLeftPosition++
-    } else if (x > initTextRight && x < endTextRight) {
-      alignedText += textRight[cursorTextRightPosition]
-      cursorTextRightPosition++
-    } else {
-      alignedText += '-'
+    for (let x = 0; x < lineSize; x++) {
+      if (x >= 0 && x < textLeft.length) {
+        alignedText += textLeft[cursorTextLeftPosition]
+        cursorTextLeftPosition++
+      } else if (x > initTextRight && x < endTextRight) {
+        alignedText += textRight[cursorTextRightPosition]
+        cursorTextRightPosition++
+      } else {
+        alignedText += delimiter
+      }
     }
-  }
 
-  return alignedText
+    return alignedText
+  }
 }
+
+module.exports = new Align()
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,25 +1,25 @@
-import * as align from '../'
+import align from '../'
 
 describe('Align Text', () => {
   const text = "TEXT TO ALIGN"
 
   it('Left Align', async () => {
-    const textAligned = align.leftAligned(text, 21)
+    const textAligned = align.leftAligned(text, '-', 21)
     expect(textAligned).toBe('TEXT TO ALIGN--------')
   })
 
   it('Align Right', async () => {
-    const textAligned = align.rightAligned(text, 21)
+    const textAligned = align.rightAligned(text, '-', 21)
     expect(textAligned).toBe('--------TEXT TO ALIGN')
   })
 
   it('Center Align', async () => {
-    const textAligned = align.centralized(text, 21)
+    const textAligned = align.centralized(text, '-', 21)
     expect(textAligned).toBe('----TEXT TO ALIGN----')
   })
 
   it('Should align firt word on left and next word on right', async () => {
-    const textAligned = align.leftRightAligned('bola', 'carro', 15)
+    const textAligned = align.leftRightAligned('bola', 'carro', '-', 15)
     expect(textAligned).toBe('bola------carro')
   })
 })
